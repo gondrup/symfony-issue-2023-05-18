8a84bcf04568f8780b883c50d661b6fb2e3dfe15
Adrian Harding <adrian@theaacars.com>
2023-05-18 11:55:34 +0100

    Add basic example that works as expected

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
     #[Route('/comment', name: 'app_comment')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Add to database or something
+        }
+
         return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+            'form' => $form,
+            'comment' => $comment,
         ]);
     }
 }
